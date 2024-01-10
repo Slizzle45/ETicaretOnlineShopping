5d04bfff7cf5aa884531340c9f266c577af10467
efe corukoğlu <efecorukoglu@gmail.com>
2024-01-10 17:54:35 +0300

    Register and Payment page added

---
+++ ETicaret.Web/Controllers/PaymentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaret.Web.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public IActionResult Payment()
+        {
+            return View();
+        }
+    }
+}
--- ETicaret.Web/wwwroot/js/scripts.js
+++ ETicaret.Web/wwwroot/js/scripts.js
@@ -5,3 +5,11 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+(function () {
+
+    $("#cart").on("click", function () {
+        $(".shopping-cart").fadeToggle("fast");
+    });
+
+})();
