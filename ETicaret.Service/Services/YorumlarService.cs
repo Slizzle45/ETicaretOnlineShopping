be0056d5e36f4d773b70bab65717959b0427bfe7
efe corukoğlu <efecorukoglu@gmail.com>
2024-01-10 18:21:06 +0300

    Services added Dependancy injection

---
+++ ETicaret.Service/Services/YorumlarService.cs
@@ -0,0 +1,20 @@
+﻿using ETicaret.Core.ETicaretDatabase;
+using ETicaret.Core.IRepositories;
+using ETicaret.Core.IService;
+using ETicaret.Core.IUnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaret.Service.Services
+{
+    public class YorumlarService : Service<Yorumlar>, IYorumlarService
+    {
+        public YorumlarService(IGenericRepository<Yorumlar> repository, IUnitOfWork unitOfWork) : base(repository, unitOfWork)
+        {
+
+        }
+    }
+}
--- ETicaret.Web/Program.cs
+++ ETicaret.Web/Program.cs
@@ -1,3 +1,9 @@
+using ETicaret.Core.IRepositories;
+using ETicaret.Core.IService;
+using ETicaret.Core.IUnitOfWork;
+using ETicaret.Repository.Repositories;
+using ETicaret.Repository.UntiOfWork;
+using ETicaret.Service.Services;
 namespace ETicaret.Web
 {
     public class Program
@@ -9,10 +15,23 @@ namespace ETicaret.Web
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
+            #region E Ticaret Service'leri
+
+            builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            builder.Services.AddScoped(typeof(IService<>), typeof(Service<>));
+            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+            builder.Services.AddScoped<IUrunlerRepository, UrunlerRepository>();
+            builder.Services.AddScoped<IMenulerRepository, MenulerRepository>();
+
+            //Urunler,Kategoriler, Yorumlar, Fotograflar,...
+
+            // Urunler, Kategoriler
+            #endregion
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
+            if(!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
             }
