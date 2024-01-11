ffae88da84299c8eba7bbd33bb6b07c543626f95
efe corukoğlu <efecorukoglu@gmail.com>
2024-01-11 16:55:35 +0300

    Adresler service added

---
+++ ETicaret.Service/Services/AdreslerService.cs
@@ -0,0 +1,29 @@
+﻿using ETicaret.Core.ETicaretDatabase;
+using ETicaret.Core.IRepositories;
+using ETicaret.Core.IService;
+using ETicaret.Core.IUnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaret.Service.Services
+{
+    public class AdreslerService : Service<Adresler>, IAdreslerService
+    {
+        public AdreslerService(IGenericRepository<Adresler> repository, IUnitOfWork unitOfWork) : base(repository, unitOfWork)
+        {
+        }
+
+        public Task<Adresler> GetAdreslerWithIlceAsync(int ilceId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Adresler>> GetAdreslerWithMusteriAsync(int musteriId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
