c1a975db8b95e22122214c2894a3a1c5fc14ac4a
efe corukoğlu <efecorukoglu@gmail.com>
2024-01-15 13:37:32 +0300

    added javascript to item cards, cart button

---
+++ ETicaret.Web/wwwroot/js/itemCard.js
@@ -0,0 +1,44 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+
+    var itemCard = document.querySelectorAll('.card');
+
+    function showSuccessAlert() {
+        var alertElement = document.createElement('div');
+        alertElement.className = 'alert alert-success custom-alert';
+        alertElement.setAttribute('role', 'alert');
+        alertElement.textContent = 'Ürün sepete eklendi';
+
+        document.body.appendChild(alertElement);
+
+        setTimeout(function () {
+            alertElement.remove();
+        }, 3000);
+    }
+
+
+    itemCard.forEach(function (item) {
+        item.addEventListener('click', function () {
+            window.location.href = 'ItemDetails/ItemDetails';
+        });
+
+        var cardFooter = item.querySelector('.card-footer');
+
+        cardFooter.addEventListener('click', function (event) {
+
+            event.stopPropagation();
+            console.log('alert göster');
+            showSuccessAlert();
+        });
+
+
+        item.addEventListener('mouseover', function () {
+            item.style.cursor = 'pointer';
+        });
+
+        item.addEventListener('mouseout', function () {//mouse çıktığında eski haline döner
+            item.style.cursor = 'auto';
+        });
+    });
+
+    showSuccessAlert();
+});
