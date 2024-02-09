4f3aa6d4349d0b0852f1c2927ec87548f6b54c7d
efe corukoğlu <efecorukoglu@gmail.com>
2024-02-09 20:07:52 +0300

    model added for homepage item show

--- ETicaret.Web/Controllers/HomeController.cs
+++ ETicaret.Web/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using ETicaret.Web.Models;
+﻿using ETicaret.Core.ETicaretDatabase;
+using ETicaret.Core.IService;
+using ETicaret.Service.Services;
+using ETicaret.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,15 +10,32 @@ namespace ETicaret.Web.Controllers
     public class HomeController : BaseDefaultController
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IService<Kategoriler> _kategoriService;
+        private readonly IService<Urunler> _urunService;
+        private readonly IService<Fotograflar> _fotografService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IService<Fotograflar> fotografService, IService<Urunler> urunService, IService<Kategoriler> kategoriService)
         {
             _logger = logger;
+            _fotografService = fotografService;
+            _urunService = urunService;
+            _kategoriService = kategoriService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var kategoriler = await _kategoriService.GetAllAsyncs();
+            var urunler = await _urunService.GetAllAsyncs();
+            var fotograflar = await _fotografService.GetAllAsyncs();
+
+            var model = new UrunlerViewModel
+            {
+                Kategoriler = kategoriler,
+                Urunler = urunler,
+                Fotograflar = fotograflar
+            };
+
+            return View(model);
         }
 
         public IActionResult Register()
---
+++ ETicaret.Web/Models/UrunlerViewModel.cs
@@ -0,0 +1,11 @@
+﻿using ETicaret.Core.ETicaretDatabase;
+
+namespace ETicaret.Web.Models
+{
+    public class UrunlerViewModel
+    {
+        public IEnumerable<Urunler> Urunler { get; set; }
+        public IEnumerable<Kategoriler> Kategoriler { get; set; }
+        public IEnumerable<Fotograflar> Fotograflar { get; set; }
+    }
+}
